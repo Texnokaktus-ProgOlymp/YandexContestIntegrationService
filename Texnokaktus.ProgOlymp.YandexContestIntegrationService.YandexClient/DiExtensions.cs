de8e819ac92cdfaa33ef30d81a936993a00fe2bd
Artem Krylov <kav128@outlook.com>
2025-04-10 14:26:33 +0300

    finally remove old Yandex Contest client

--- Texnokaktus.ProgOlymp.YandexContestIntegrationService.YandexClient/DiExtensions.cs
+++ Texnokaktus.ProgOlymp.YandexContestIntegrationService.YandexClient/DiExtensions.cs
@@ -1,13 +1,8 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Kiota.Abstractions;
 using Microsoft.Kiota.Abstractions.Authentication;
 using Microsoft.Kiota.Http.HttpClientLibrary;
 using RestSharp;
-using RestSharp.Authenticators;
-using RestSharp.Authenticators.OAuth2;
-using RestSharp.Serializers.Json;
 using Texnokaktus.ProgOlymp.YandexContestIntegrationService.YandexClient.Models;
 using Texnokaktus.ProgOlymp.YandexContestIntegrationService.YandexClient.Services;
 using Texnokaktus.ProgOlymp.YandexContestIntegrationService.YandexClient.Services.Abstractions;
@@ -20,30 +15,8 @@ public static class DiExtensions
     public static IServiceCollection AddYandexClientServices(this IServiceCollection services) =>
         services.AddScoped<IYandexAuthenticationService, YandexAuthenticationService>()
                 .AddScoped<ITokenService, TokenService>()
-                .AddScoped<IContestClient, Services.ContestClient>()
                 .AddScoped<IAuthenticationProvider, YandexOAuthAuthenticationProvider>()
                 .AddScoped<IRequestAdapter, HttpClientRequestAdapter>()
                 .AddScoped<ContestClient>()
-                .AddScoped<IAuthenticator>(provider =>
-                 {
-                     var tokenService = provider.GetRequiredService<ITokenService>();
-                     var accessToken = tokenService.GetAccessTokenAsync()
-                                                   .GetAwaiter()
-                                                   .GetResult()
-                                    ?? throw new("No token");
-                     return new OAuth2AuthorizationRequestHeaderAuthenticator(accessToken);
-                 })
-                .AddKeyedScoped<IRestClient>(ClientType.YandexContest,
-                                             (provider, _) =>
-                                                 new RestClient("https://api.contest.yandex.net/api/public/v2/",
-                                                                options => options.Authenticator = provider.GetRequiredService<IAuthenticator>(),
-                                                                configureSerialization: config =>
-                                                                    config.UseSystemTextJson(new(JsonSerializerDefaults.Web)
-                                                                    {
-                                                                        Converters =
-                                                                        {
-                                                                            new JsonStringEnumConverter()
-                                                                        }
-                                                                    })))
-                .AddKeyedScoped<IRestClient>(ClientType.YandexOAuth, (_, _) => new RestClient("https://oauth.yandex.ru"));
+                .AddScoped<IRestClient>(_ => new RestClient("https://oauth.yandex.ru"));
 }
